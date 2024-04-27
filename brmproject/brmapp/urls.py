0dcd3c68648e7eef47a1226bcfbbb743d7d77bae
muskanshah04 <142525254+muskanshah04@users.noreply.github.com>
2024-04-27 18:38:51 +0530

    Add files via upload

---
+++ brmproject/brmapp/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path,include
+from .views import helloView,addBookView,addBook,editBook,editBookView,deleteBookView
+
+urlpatterns = [
+    path("",helloView),
+    path("add-book/",addBookView),
+    path("add-book/add",addBook),
+    path("edit-book/",editBookView),
+    path("edit-book/edit",editBook),
+    path("delete-book/",deleteBookView)
+    
+    
+]
